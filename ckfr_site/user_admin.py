ba87653439080d55d2e5297d0ecc49e7c7c5394d
zarilo88-ui <zarilo88@gmail.com>
2025-10-03 12:48:56 +0000

    Admin: unregister default UserAdmin and mask password with ********

--- ckfr_site/user_admin.py
+++ ckfr_site/user_admin.py
@@ -3,18 +3,28 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django import forms
 
+# Always show ******** instead of the hashed password block
 class MaskedPasswordWidget(forms.Widget):
     def render(self, name, value, attrs=None, renderer=None):
         return "********"
 
 class CustomUserChangeForm(forms.ModelForm):
+    # Override the password field to prevent ReadOnlyPasswordHashField
+    password = forms.CharField(label="Mot de passe", required=False, widget=MaskedPasswordWidget())
+
     class Meta:
         model = User
         fields = "__all__"
-        widgets = {"password": MaskedPasswordWidget()}
+
+# Unregister the default admin first (avoids AlreadyRegistered)
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     form = CustomUserChangeForm
+    # keep default layouts
     fieldsets = BaseUserAdmin.fieldsets
     add_fieldsets = BaseUserAdmin.add_fieldsets
